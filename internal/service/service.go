10db20f2e36b0a7539afdae18f51bbad4fb35822
Ivan Kudryavtsev <kudryavcev.ivan17@rwb.ru>
2025-11-12 23:03:53 +0300

    change some thingies

--- internal/service/service.go
+++ internal/service/service.go
@@ -6,42 +6,152 @@ import (
 	"github.com/kxddry/avito-backend-internship-2025/internal/domain"
 )
 
-type Dependencies struct{}
+type PullRequestRepository interface {
+	Create(ctx context.Context, pr domain.PullRequest) error
+	GetByID(ctx context.Context, pullRequestID string) (domain.PullRequest, error)
+	Update(ctx context.Context, pr domain.PullRequest) error
+	Delete(ctx context.Context, pullRequestID string) error
+}
+
+type TeamRepository interface {
+	Create(ctx context.Context, team domain.Team) error
+	GetByName(ctx context.Context, teamName string) (domain.Team, error)
+	Update(ctx context.Context, team domain.Team) error
+	Delete(ctx context.Context, teamName string) error
+}
+
+type UserRepository interface {
+	Create(ctx context.Context, user domain.User) error
+	GetByID(ctx context.Context, userID string) (domain.User, error)
+	Update(ctx context.Context, user domain.User) error
+	Delete(ctx context.Context, userID string) error
+}
+
+type RepositoryProvider interface {
+	PullRequests() PullRequestRepository
+	Teams() TeamRepository
+	Users() UserRepository
+}
+
+type TxManager interface {
+	WithTx(ctx context.Context, fn func(context.Context, RepositoryProvider) error) error
+	WithROTx(ctx context.Context, fn func(context.Context, RepositoryProvider) error) error
+}
+
+type Storage struct {
+	PullRequestRepo PullRequestRepository
+	TeamRepo        TeamRepository
+	UserRepo        UserRepository
+}
+
+func (s Storage) PullRequests() PullRequestRepository {
+	return s.PullRequestRepo
+}
+
+func (s Storage) Teams() TeamRepository {
+	return s.TeamRepo
+}
+
+func (s Storage) Users() UserRepository {
+	return s.UserRepo
+}
+
+type Dependencies struct {
+	Repositories       RepositoryProvider
+	TransactionManager TxManager
+}
 
 type Service struct {
-	deps Dependencies
+	repos RepositoryProvider
+	tx    TxManager
 }
 
 func New(deps Dependencies) *Service {
-	return &Service{deps: deps}
+	repos := deps.Repositories
+	if repos == nil {
+		repos = Storage{}
+	}
+
+	tx := deps.TransactionManager
+	if tx == nil {
+		tx = noopTxManager{repos: repos}
+	}
+
+	return &Service{
+		repos: repos,
+		tx:    tx,
+	}
 }
 
 var _ domain.AssignmentService = (*Service)(nil)
 
 func (s *Service) CreatePullRequest(ctx context.Context, input domain.CreatePullRequestInput) (domain.PullRequest, error) {
-	return domain.PullRequest{}, domain.ErrNotImplemented
+	var result domain.PullRequest
+	err := s.tx.WithTx(ctx, func(ctx context.Context, repos RepositoryProvider) error {
+		return domain.ErrNotImplemented
+	})
+	return result, err
 }
 
 func (s *Service) MergePullRequest(ctx context.Context, input domain.MergePullRequestInput) (domain.PullRequest, error) {
-	return domain.PullRequest{}, domain.ErrNotImplemented
+	var result domain.PullRequest
+	err := s.tx.WithTx(ctx, func(ctx context.Context, repos RepositoryProvider) error {
+		return domain.ErrNotImplemented
+	})
+	return result, err
 }
 
 func (s *Service) ReassignPullRequest(ctx context.Context, input domain.ReassignPullRequestInput) (domain.ReassignPullRequestResult, error) {
-	return domain.ReassignPullRequestResult{}, domain.ErrNotImplemented
+	var result domain.ReassignPullRequestResult
+	err := s.tx.WithTx(ctx, func(ctx context.Context, repos RepositoryProvider) error {
+		return domain.ErrNotImplemented
+	})
+	return result, err
 }
 
 func (s *Service) UpsertTeam(ctx context.Context, team domain.Team) (domain.Team, error) {
-	return domain.Team{}, domain.ErrNotImplemented
+	var result domain.Team
+	err := s.tx.WithTx(ctx, func(ctx context.Context, repos RepositoryProvider) error {
+		return domain.ErrNotImplemented
+	})
+	return result, err
 }
 
 func (s *Service) GetTeam(ctx context.Context, teamName string) (domain.Team, error) {
-	return domain.Team{}, domain.ErrNotImplemented
+	var result domain.Team
+	err := s.tx.WithROTx(ctx, func(ctx context.Context, repos RepositoryProvider) error {
+		return domain.ErrNotImplemented
+	})
+	return result, err
 }
 
 func (s *Service) GetReviewerAssignments(ctx context.Context, userID string) (domain.ReviewerAssignments, error) {
-	return domain.ReviewerAssignments{}, domain.ErrNotImplemented
+	var result domain.ReviewerAssignments
+	err := s.tx.WithROTx(ctx, func(ctx context.Context, repos RepositoryProvider) error {
+		return domain.ErrNotImplemented
+	})
+	return result, err
 }
 
 func (s *Service) SetUserIsActive(ctx context.Context, input domain.SetUserIsActiveInput) (domain.User, error) {
-	return domain.User{}, domain.ErrNotImplemented
+	var result domain.User
+	err := s.tx.WithTx(ctx, func(ctx context.Context, repos RepositoryProvider) error {
+		return domain.ErrNotImplemented
+	})
+	return result, err
+}
+
+type noopTxManager struct {
+	repos RepositoryProvider
+}
+
+func (n noopTxManager) WithTx(ctx context.Context, fn func(context.Context, RepositoryProvider) error) error {
+	if fn == nil {
+		return nil
+	}
+	return fn(ctx, n.repos)
+}
+
+func (n noopTxManager) WithROTx(ctx context.Context, fn func(context.Context, RepositoryProvider) error) error {
+	return n.WithTx(ctx, fn)
 }
--- migrations/003_create_pull_requests.up.sql
+++ migrations/003_create_pull_requests.up.sql
@@ -1,8 +1,8 @@
 CREATE TYPE pr_status AS ENUM ('OPEN', 'MERGED');
 
 CREATE TABLE IF NOT EXISTS pull_requests (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    title TEXT NOT NULL,
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
     author_id UUID REFERENCES users(id),
     status pr_status NOT NULL DEFAULT 'OPEN',
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
